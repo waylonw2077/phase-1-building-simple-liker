59f258f699a18b3d1f0cfd998d3b58d124d55ec6
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-03-27 15:40:39 -0400

    completes lab

--- main.js
+++ main.js
@@ -3,7 +3,45 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModal = document.querySelector('#modal')
+const errorMessage = document.querySelector('#modal-message')
 
+function mimicServerCall() {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      const success = Math.random() < 0.5
+      if (success) {
+        resolve('Pretend remote server notified of action!')
+      } else {
+        reject('Something went wrong')
+      }
+    }, 300)
+  })
+}
+
+function toggleHeart(event) {
+  const heart = event.target
+  if (heart.innerText === EMPTY_HEART) {
+    mimicServerCall()
+      .then(() => {
+        heart.innerText = FULL_HEART
+        heart.classList.add('activated-heart')
+      })
+      .catch((error) => {
+        errorMessage.innerText = error
+        errorModal.classList.remove('hidden')
+        setTimeout(() => {
+          errorModal.classList.add('hidden')
+        }, 3000)
+      })
+  } else if (heart.innerText === FULL_HEART) {
+    heart.innerText = EMPTY_HEART
+    heart.classList.remove('activated-heart')
+  }
+}
+
+const hearts = document.querySelectorAll('.like-glyph')
+hearts.forEach(heart => heart.addEventListener('click', toggleHeart))
 
 
 
